ee94b266f2555024113ac6e524c4dfe6740430ca
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-21 20:28:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-// import { Statistics } from "./Statistics/Statistics";
+import { Statistics } from "./Statistics/Statistics";
 // import { Section } from "./Section/Section";
 // import { Notification } from "./Notification/Notification";
 
@@ -7,18 +7,20 @@
 export const App = () => {
   return (
     <div>
-      {/* <Section title="Please leave feedback">
+      {/* 
+      <Section title="Please leave feedback">
       </Section>
       <Notification />
       <FeedbackOptions 
       />
+    */}
       <Statistics 
       good={0}
       neutral={0}
       bad={0}
       total={0}
       positivePercentage={0}
-      /> */}
+      /> 
     </div>
   );
 };
