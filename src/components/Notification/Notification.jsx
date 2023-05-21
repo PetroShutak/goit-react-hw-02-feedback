eca2b427f9b9e637ef6c27d443dc958f4777ad42
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-22 00:56:51 +0300

    added getRandom to notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import css from './Notification.module.css';
+import { getRandomColor } from '../../utils/getRandom';
 
 export const Notification = ({ message }) => (
-  <div className={css.notification}>
+  <div className={css.notification} style={{backgroundColor:  getRandomColor()}}>
     <p>{message}</p>
   </div>
 );
