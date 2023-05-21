bb28fe905821785e02b43e840a998bdfc329e33b
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-21 23:05:01 +0300

    added useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,44 @@
-// import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { useState } from "react";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
-// import { Section } from "./Section/Section";
-// import { Notification } from "./Notification/Notification";
-
+import { Section } from "./Section/Section";
+import { Notification } from "./Notification/Notification";
 
 export const App = () => {
+  const [showNotification, setShowNotification] = useState(false);
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleFeedback = (option) => {
+    setShowNotification(true);
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1
+    }));
+  };
+
+  const { good, neutral, bad } = feedback;
+
   return (
     <div>
-      {/* 
       <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
       </Section>
-      <Notification />
-      <FeedbackOptions 
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={good + neutral + bad}
+        positivePercentage={Math.round((good / (good + neutral + bad)) * 100)}
       />
-    */}
-      <Statistics 
-      good={0}
-      neutral={0}
-      bad={0}
-      total={0}
-      positivePercentage={0}
-      /> 
+      {showNotification && <Notification message="Дякуємо за ваш відгук!" />}
     </div>
   );
 };
+
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 export const Section = ({ title, children }) => (
   <div>
     <h2>{title}</h2>
-  </div>
-);
-
-export const Children = ({ children }) => (
-  <div>
     {children}
   </div>
 );
+
+
 Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
